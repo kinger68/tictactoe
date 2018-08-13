3100ca0f07ee365adcb14ec6cc714de42c946f25
Lawrence King <lawrence.king@hds.com>
2018-08-13 14:59:14 -0400

    Game recognizes that X has won.

--- src/com/lking/tictactoe/Board.java
+++ src/com/lking/tictactoe/Board.java
@@ -2,6 +2,7 @@ package com.lking.tictactoe;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public class Board {
     private Set<Square> takenSquares = null;
@@ -26,5 +27,19 @@ public class Board {
 
     public boolean isFull() {
         return takenSquares.size() == 9;
+    }
+
+    public boolean hasWon() {
+        var winningCombos = Stream.of(
+                Stream.of(Square.TOP_LEFT, Square.TOP_CENTER, Square.TOP_RIGHT),
+                Stream.of(Square.MID_LEFT, Square.MID_CENTER, Square.MID_RIGHT),
+                Stream.of(Square.BOTTOM_LEFT, Square.BOTTOM_CENTER, Square.BOTTOM_RIGHT),
+                Stream.of(Square.TOP_LEFT, Square.MID_LEFT, Square.BOTTOM_LEFT),
+                Stream.of(Square.TOP_CENTER, Square.MID_CENTER, Square.BOTTOM_CENTER),
+                Stream.of(Square.TOP_RIGHT, Square.MID_RIGHT, Square.BOTTOM_RIGHT),
+                Stream.of(Square.TOP_LEFT, Square.MID_CENTER, Square.BOTTOM_RIGHT),
+                Stream.of(Square.BOTTOM_LEFT, Square.MID_CENTER, Square.TOP_RIGHT)
+        );
+        return winningCombos.anyMatch(combo -> combo.allMatch(square -> takenSquares.contains(square)));
     }
 }
